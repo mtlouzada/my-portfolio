0f274ec9bbb3ad6079cdcd18ced7e941ac292ca9
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-05 23:32:25 -0300

    update skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-
+      <div className="header">
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -11,6 +11,16 @@ export default function Home() {
               height={24}
               priority
             />
+        <div>
+          <h1>Hi, I´m Matheus! 🛹</h1>
+          <h2>Web Developer</h2>
+        </div>
+      </div>
+        
+        <div className="experience">
+          <h3>Experience</h3>
+          <p>texto</p>
+        </div>
 
     </main>
   );
