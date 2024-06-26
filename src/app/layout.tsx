34c59a8b52c391952e649496f563fbfbbb406b59
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-26 19:05:16 -0300

    feat: config experience section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.scss";
 
-const inter = Poppins({ subsets: ['latin'], weight: ['400', '500', '600'] });
+const inter = Poppins({ subsets: ['latin'], weight: ['300', '400', '500', '600'] });
 
 export const metadata: Metadata = {
   title: "Matheus Louzada",
