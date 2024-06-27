a590129ee15b91ffebd52aec9ce539e7407eb805
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-27 11:51:44 -0300

    bug fixed information component

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -5,13 +5,12 @@ export function Information() {
         <div className="infos">
             <span>🇺🇸 EN Intermediary</span>
             <span>🇧🇷 PT BR Native Speaker</span>
-            <h3>Education</h3>
+            
             <div className="educational-info">
+                <h3>Education</h3>
                 <span>🎓</span>
                 <span>Systems Analysis and Development - Universidade Estásio de Sá</span>
-                <div className="buttons">
-                    <div className="social"></div>
-                </div>
             </div>
+        </div>
             );
 }
