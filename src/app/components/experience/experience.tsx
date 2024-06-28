d885713b68b7915bb67d68d22d9a5b9721c9192c
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-06-27 21:54:52 -0300

    feat: create experience part and start config

--- src/app/components/experience/experience.tsx
+++ src/app/components/experience/experience.tsx
@@ -1,17 +1,53 @@
 import { SectionTitle } from "../sectionTitle/sectionTitle";
 import "./experience.scss";
+import Image from "next/image";
 
 export function Experience() {
     return (
-        
-            <div className="experience">
-                <SectionTitle text="Experience"/>
-                <p>Tech enthusiast</p>
-                <div className="experience-time">
 
+        <div className="experience">
+            <SectionTitle text="Experience" />
+            <p>2 years doing projects putting into practice the knowledge of college and an extracurricular course</p>
+            <div className="experience-time">
+                <div>
+                    <Image
+                        src="/react-icon.png"
+                        alt="React Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
                 </div>
-
+                <div>
+                    <Image
+                        src="/js-icon.png"
+                        alt="JavaScript Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                </div>
+                <div>
+                    <Image
+                        src="/ts-icon.png"
+                        alt="TypeScript Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                </div>
+                <div>
+                    <Image
+                        src="/Java-icon.png"
+                        alt="Java Logo"
+                        width={40}
+                        height={40}
+                        priority
+                    />
+                </div>
             </div>
-        
+
+        </div>
+
     )
 }
