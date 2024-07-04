94d5d3d56e289db7f1b5b0f043c27c34208c6bd2
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-03 22:03:30 -0300

    feat: delete old folders

--- src/app/components/information/information.tsx
+++ src/app/components/information/information.tsx
@@ -1,17 +1,16 @@
-import { SectionTitle } from "../sectionTitle/sectionTitle";
 import "./information.scss";
 
 export function Information() {
     return (
         <div className="infos">
             
-            <SectionTitle text="Languages"/>
+           
             <div className="languages-info">
                 <span>🇺🇸 EN Intermediary</span>
                 <span>🇧🇷 PT BR Native Speaker</span>
             </div>
 
-            <SectionTitle text="Education"/>
+            
             <div className="educational-info">
                 <span>🎓</span>
                 <span>Systems Analysis and Development - Universidade Estásio de Sá</span>
