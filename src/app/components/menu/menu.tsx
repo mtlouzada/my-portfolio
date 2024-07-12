1038f572c673e9d8afff97e8617e8b2c049f5c04
LouzadaRJ <matheuslouzadaa@gmail.com>
2024-07-12 11:41:07 -0300

    feat: create links for menu bar, and start style syntax

--- src/app/components/menu/menu.tsx
+++ src/app/components/menu/menu.tsx
@@ -1,8 +1,26 @@
+import { SocialBtns } from '../social-btns/social-btns';
 import './menu.scss';
 
 export function Menu() {
     return(
-        <div className='container-menu'>Menu bar</div>
+        <div className='container-menu'>
+            
+            <div className='Logo'>
+                <a href="mailto">Email.code</a>
+                <button>PT-BR</button>
+            </div>
+
+            <div className="nav-bar">
+                <a href="">Sobre</a>
+                <a href="">Habilidades</a>
+                <a href="">Projetos</a>
+                <a href="">Contado</a>
+            </div>
+
+            <div className="Redes-sociais">
+                <SocialBtns/>
+            </div>
+        </div>
 
     );
 }
--- src/app/components/social-btns/social-btns.tsx
+++ src/app/components/social-btns/social-btns.tsx
@@ -8,18 +8,14 @@ import "./social-btns.scss"
 export function SocialBtns() {
     return(
         <div className="social">
-            <a href="https://www.instagram.com/mt.louzada/" target="black">
-                <InstaIcon/>
-            </a>
+            
             <a href="https://www.linkedin.com/in/matheus-louzadaa/" target="black">
                 <LinkedinIcon/>
             </a>
             <a href="https://github.com/mtlouzada" target="black">
                 <GithubIcon/>
             </a>
-            <a href="https://x.com/mt_lou_zada" target="black">
-                <TwitterIcon/>
-            </a>
+            
         </div>
     )
 }
