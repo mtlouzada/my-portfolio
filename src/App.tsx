08e1744e892aa2afc0a5599e4bf8d32d821a4f68
mt.louzada <matheuslouzadaa@gmail.com>
2024-10-21 23:23:00 -0300

    new: container for bio text

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
         <header id='about' className=''>
 
           <div className='menu-bar'>
+
             <div className='left-nav-bar mouse-transition'>
               <a href="mailto:matheuslouzadaa@gmail.com" target='black'><MdOutlineMailOutline />matheuslouzadaa</a>
             </div>
@@ -27,21 +28,25 @@ function App() {
               <a href="https://www.linkedin.com/in/matheus-louzadaa/" target='black'><FaLinkedin /></a>
               <a href="https://github.com/mtlouzada" target='black'><FaGithub /></a>
             </div>
+
           </div>
 
-          <div>
+          <div className='bio-container'>
             <img src="./ft-perfil.jpg" alt="Foto Perfil"/>
-            <h1>Matheus Louzada</h1>
-            <h3>Desenvolvedor web</h3>
 
-            <p>
-            Me formei em ADS no fim de 2023, desde então estou fazendo cursos profissionalizantes focado no desenvolvimento Web2. Também participei de um BootCamp seguido de um Hackathon na ICP HUB Brasil, onde tive a experiência de desenvolver um projeto em equipe e apresentá-lo.
+            <div className='text-container'>
+              <h1>Matheus Louzada</h1>
+              <h3>Desenvolvedor web</h3>
 
-  Estudo sobre programação diariamente, me desafiando a criar projetos completos, utilizando bibliotecas, frameworks e banco de dados para isso. Desenvolvendo autonomia na criação de código e aprimorando o meu raciocínio lógico. Visando ser capaz de entregar soluções tecnológicas de forma rápida e eficaz.
-            </p>
+              <p>
+              Me formei em ADS no fim de 2023, desde então estou fazendo cursos profissionalizantes focado no desenvolvimento Web2. Também participei de um BootCamp seguido de um Hackathon na ICP HUB Brasil, onde tive a experiência de desenvolver um projeto em equipe e apresentá-lo.
 
-            <a href="">Ver Currículo</a>
-            <a href="">Enviar Email</a>
+              Estudo sobre programação diariamente, me desafiando a criar projetos completos, utilizando bibliotecas, frameworks e banco de dados para isso. Desenvolvendo autonomia na criação de código e aprimorando o meu raciocínio lógico. Visando ser capaz de entregar soluções tecnológicas de forma rápida e eficaz.
+              </p>
+
+              <a href="">Ver Currículo</a>
+              <a href="">Enviar Email</a>
+            </div>
 
           </div>
 
