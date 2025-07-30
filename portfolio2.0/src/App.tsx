8c3b24064b3a5df5f7c9ec9252b8df6c661428cd
mt.louzada <matheuslouzadaa@gmail.com>
2025-07-30 16:47:25 -0300

    feat: git teste

--- portfolio2.0/src/App.tsx
+++ portfolio2.0/src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    {/* teste git */}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
