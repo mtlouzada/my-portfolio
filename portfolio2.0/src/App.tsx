63670c23261fd9cd77ce2e5fd082110452a11e75
mt.louzada <matheuslouzadaa@gmail.com>
2025-07-31 13:53:28 -0300

    cleanup: delete unusual lines and refactor hmtl structure

--- portfolio2.0/src/App.tsx
+++ portfolio2.0/src/App.tsx
@@ -1,4 +1,5 @@
-import './App.css'
+import React from 'react';
+import './styles/index.css';
 
 function App() {
 
@@ -34,7 +35,7 @@ function App() {
                 </a>
 
                 <a className="nav-close-btn" aria-label="close menu" data-nav-toggler>
-                  <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+                  {/* <ion-icon name="close-outline" aria-hidden="true"></ion-icon> */}
                 </a>
 
               </div>
@@ -67,7 +68,7 @@ function App() {
             </nav>
 
             <button className="nav-open-btn" aria-label="open menu" data-nav-toggler>
-              <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+              {/* <ion-icon name="menu-outline" aria-hidden="true"></ion-icon> */}
             </button>
 
             <div className="overlay" data-nav-toggler data-overlay></div>
@@ -112,7 +113,7 @@ function App() {
 
                       <span className="span">VER CURRÍCULO</span>
 
-                      <ion-icon name="download-outline" aria-hidden="true"></ion-icon>
+                      {/* <ion-icon name="download-outline" aria-hidden="true"></ion-icon> */}
 
 
                     </a>
@@ -150,7 +151,7 @@ function App() {
                     <div className="blog-card fade-in">
 
                       <a href="https://github.com/mtlouzada/turing-Challenger" target="_blank">
-                        <figure className="card-banner img-holder" style="--width: 650; --height: 470;">
+                        <figure className="card-banner img-holder" style={{ "--width": 650, "--height": 470 } as React.CSSProperties}>
                           <img src="./src/imgs/turingchallenger.png" width="650" height="470" loading="lazy"
                             alt="Project-1" className="img-cover" />
                         </figure>
@@ -168,7 +169,7 @@ function App() {
 
                               <span className="span">IR AO PROJETO</span>
 
-                              <ion-icon name="arrow-forward"></ion-icon>
+                              {/* <ion-icon name="arrow-forward"></ion-icon> */}
 
                             </a>
                           </div>
@@ -183,7 +184,7 @@ function App() {
                     <div className="blog-card fade-in">
 
                       <a href="https://github.com/mtlouzada/salary-VS-inflation" className="card-banner" target="_blank">
-                        <figure className="img-holder" style="--width: 650; --height: 470;">
+                        <figure className="img-holder" style={{ "--width": 650, "--height": 470 } as React.CSSProperties}>
 
                           <img src="./src/imgs/salarioXiflacao.png" width="650" height="470" loading="lazy"
                             alt="salarioXiflacao" className="img-cover" />
@@ -203,7 +204,7 @@ function App() {
                             <a href="https://github.com/mtlouzada/salary-VS-inflation" className="btn has-before" target="_blank">
                               <span className="span">IR AO PROJETO</span>
 
-                              <ion-icon name="arrow-forward"></ion-icon>
+                              {/* <ion-icon name="arrow-forward"></ion-icon> */}
                             </a>
                           </div>
 
@@ -246,178 +247,181 @@ function App() {
 
                     <p className="card-text">
                       - React
-                      <br/>
-                        - Node.Js
-                        <br/>
-                          - Jest.Js
-                          <br/>
-                            - Sintax
-                            <br/>
-                              - DOM
-                              <br/>
-                                - Fetch API
-                              </p>
-                            </div>
-                          </li>
-
-                          <li className="slider-item" data-slider-item>
-                            <div className="service-card">
-
-                              <div className="card-icon">
-                                <i className="fab fa-git"></i>
-                              </div>
-
-                              <h3 className="h3 card-title">VERSIONAMENTO DE CÓDIGO</h3>
-                              <p className="card-text">
-                                - Git
-                                <br />
-                                - GitHub
-                              </p>
-                            </div>
-                          </li>
-
-                          <li className="slider-item" data-slider-item>
-                            <div className="service-card">
-
-                              <div className="card-icon">
-                                <i className="fab fa-npm"></i>
-                              </div>
-
-                              <h3 className="h3 card-title">GERENCIADOR DE PACOTES E TESTES</h3>
-                              <p className="card-text">
-                                - NPM
-                                <br />
-                                - PNPM
-                              </p>
-                            </div>
-                          </li>
-
-                          <li className="slider-item" data-slider-item>
-                            <div className="service-card">
-
-                              <div className="card-icon">
-                                <i className="fa-solid fa-code"></i>
-                              </div>
-                              <h3 className="h3 card-title">OUTROS</h3>
-                              <p className="card-text">
-                                - Typescript
-                                <br/>
-                                  - Python
-                                  <br/>
-                                    - Redux
-                                    <br/>
-                                      - Vite
-                                      <br/>
-                                        - Visual Studio Code (VScode)
-                                      </p>
-                                    </div>
-                                  </li>
-
-                                </ul>
-                                <div className="slider-controls">
-                                  <button className="slider-control prev" data-slider-prev aria-label="Slide to previous item">
-                                    <div className="line"></div>
-                                    <div className="arrow"></div>
-                                  </button>
-
-                                  <button className="slider-control next" data-slider-next aria-label="Slide to next item">
-                                    <div className="line"></div>
-                                    <div className="arrow"></div>
-                                  </button>
-                                </div>
-
-                            </div>
-
-                          </div>
-                        </section>
-                      </section>
-
-                      <section className="section portfolio fade-in" aria-labelledby="portfolio-label">
-
-                        <div className="title-wrapper container">
-
-                          <div>
-                            <p className="section-subtitle" id="portfolio-label">Trabalhos Antigos</p>
-
-                            <h2 className="h2 section-title">Outros Projetos</h2>
-                          </div>
-
-                          <p className="section-text">
-                            Estes são alguns dos projetos que desenvolvi durante meu primeiro curso de programação, em uma plataforma focada em estabelecer uma base sólida nos fundamentos do desenvolvimento front-end e back-end.
-                          </p>
-                        </div>
-
-                        <div className="slider fade-in" data-slider>
-
-                          <ul className="slider-container" data-slider-container>
-
-                            <li className="slider-item">
-                              <div className="portfolio-card img-holder" style="--width: 600; --height: 600;">
-
-                                <img src="./src/imgs/coffe-house.png" width="600" height="600" loading="lazy" alt="portfolio"
-                                  className="img-cover" />
-
-                                <div className="card-content">
-                                  <h3 className="h3 card-title">Coffe House</h3>
-
-                                  <p className="card-text">Site Ecommerce</p>
-                                </div>
-
-                                <a href="https://mtlouzada.github.io/Manhattan-Coffee-House/" target="_blank" className="layer-link"></a>
-
-                              </div>
-                            </li>
-
-                            <li className="slider-item">
-                              <div className="portfolio-card img-holder" style="--width: 600; --height: 600;">
-
-                                <img src="./src/imgs/clinica.png" width="600" height="600" loading="lazy" alt="portfolio"
-                                  className="img-cover" />
-                                <div className="card-content">
-                                  <h3 className="h3 card-title">Clínica</h3>
-
-                                  <p className="card-text">Site Ecommerce</p>
-                                </div>
-                                <a href="https://mtlouzada.github.io/clinica/" target="_blank" className="layer-link"></a>
-                              </div>
-                            </li>
-
-                            <li className="slider-item">
-                              <div className="portfolio-card img-holder" style="--width: 600; --height: 600;">
-
-                                <img src="./src/imgs/host-site.png" width="600" height="600" loading="lazy" alt="portfolio"
-                                  className="img-cover" />
-
-                                <div className="card-content">
-                                  <h3 className="h3 card-title">Hospedagem</h3>
-
-                                  <p className="card-text">Site Ecommerce</p>
-                                </div>
-
-                                <a href="https://github.com/mtlouzada/Provedor" target="_blank" className="layer-link"></a>
-
-                              </div>
-                            </li>
-
-                          </ul>
-
-                          <div className="slider-controls">
-                            <button className="slider-control prev" data-slider-prev aria-label="Slide to previous item">
-                              <div className="line"></div>
-                              <div className="arrow"></div>
-                            </button>
-                            <button className="slider-control next" data-slider-next aria-label="Slide to next item">
-                              <div className="line"></div>
-                              <div className="arrow"></div>
-                            </button>
-                          </div>
-                        </div>
-                      </section>
                       <br />
-                    </article>
-                  </main>
+                      - Node.Js
+                      <br />
+                      - Jest.Js
+                      <br />
+                      - Sintax
+                      <br />
+                      - DOM
+                      <br />
+                      - Fetch API
+                    </p>
+                  </div>
+                </li>
 
-                  <a href="#theme" className="theme-btn" onclick="toggleTheme()">
+                <li className="slider-item" data-slider-item>
+                  <div className="service-card">
+
+                    <div className="card-icon">
+                      <i className="fab fa-git"></i>
+                    </div>
+
+                    <h3 className="h3 card-title">VERSIONAMENTO DE CÓDIGO</h3>
+                    <p className="card-text">
+                      - Git
+                      <br />
+                      - GitHub
+                    </p>
+                  </div>
+                </li>
+
+                <li className="slider-item" data-slider-item>
+                  <div className="service-card">
+
+                    <div className="card-icon">
+                      <i className="fab fa-npm"></i>
+                    </div>
+
+                    <h3 className="h3 card-title">GERENCIADOR DE PACOTES E TESTES</h3>
+                    <p className="card-text">
+                      - NPM
+                      <br />
+                      - PNPM
+                    </p>
+                  </div>
+                </li>
+
+                <li className="slider-item" data-slider-item>
+                  <div className="service-card">
+
+                    <div className="card-icon">
+                      <i className="fa-solid fa-code"></i>
+                    </div>
+                    <h3 className="h3 card-title">OUTROS</h3>
+                    <p className="card-text">
+                      - Typescript
+                      <br />
+                      - Python
+                      <br />
+                      - Redux
+                      <br />
+                      - Vite
+                      <br />
+                      - Visual Studio Code (VScode)
+                    </p>
+                  </div>
+                </li>
+
+              </ul>
+              <div className="slider-controls">
+                <button className="slider-control prev" data-slider-prev aria-label="Slide to previous item">
+                  <div className="line"></div>
+                  <div className="arrow"></div>
+                </button>
+
+                <button className="slider-control next" data-slider-next aria-label="Slide to next item">
+                  <div className="line"></div>
+                  <div className="arrow"></div>
+                </button>
+              </div>
+
+            </div>
+
+          </div>
+        </section>
+
+      <section className="section portfolio fade-in" aria-labelledby="portfolio-label">
+
+        <div className="title-wrapper container">
+
+          <div>
+            <p className="section-subtitle" id="portfolio-label">Trabalhos Antigos</p>
+
+            <h2 className="h2 section-title">Outros Projetos</h2>
+          </div>
+
+          <p className="section-text">
+            Estes são alguns dos projetos que desenvolvi durante meu primeiro curso de programação, em uma plataforma focada em estabelecer uma base sólida nos fundamentos do desenvolvimento front-end e back-end.
+          </p>
+        </div>
+
+        <div className="slider fade-in" data-slider>
+
+          <ul className="slider-container" data-slider-container>
+
+            <li className="slider-item">
+              <div className="portfolio-card img-holder" style={{ "--width": 600, "--height": 600 } as React.CSSProperties}>
+
+                <img src="./src/imgs/coffe-house.png" width="600" height="600" loading="lazy" alt="portfolio"
+                  className="img-cover" />
+
+                <div className="card-content">
+                  <h3 className="h3 card-title">Coffe House</h3>
+
+                  <p className="card-text">Site Ecommerce</p>
+                </div>
+
+                <a href="https://mtlouzada.github.io/Manhattan-Coffee-House/" target="_blank" className="layer-link"></a>
+
+              </div>
+            </li>
+
+            <li className="slider-item">
+              <div className="portfolio-card img-holder" style={{ "--width": 600, "--height": 600 } as React.CSSProperties}>
+
+                <img src="./src/imgs/clinica.png" width="600" height="600" loading="lazy" alt="portfolio"
+                  className="img-cover" />
+                <div className="card-content">
+                  <h3 className="h3 card-title">Clínica</h3>
+
+                  <p className="card-text">Site Ecommerce</p>
+                </div>
+                <a href="https://mtlouzada.github.io/clinica/" target="_blank" className="layer-link"></a>
+              </div>
+            </li>
+
+            <li className="slider-item">
+              <div className="portfolio-card img-holder" style={{ "--width": 600, "--height": 600 } as React.CSSProperties}>
+
+                <img src="./src/imgs/host-site.png" width="600" height="600" loading="lazy" alt="portfolio"
+                  className="img-cover" />
+
+                <div className="card-content">
+                  <h3 className="h3 card-title">Hospedagem</h3>
+
+                  <p className="card-text">Site Ecommerce</p>
+                </div>
+
+                <a href="https://github.com/mtlouzada/Provedor" target="_blank" className="layer-link"></a>
+
+              </div>
+            </li>
+
+          </ul>
+
+          <div className="slider-controls">
+            <button className="slider-control prev" data-slider-prev aria-label="Slide to previous item">
+              <div className="line"></div>
+              <div className="arrow"></div>
+            </button>
+            <button className="slider-control next" data-slider-next aria-label="Slide to next item">
+              <div className="line"></div>
+              <div className="arrow"></div>
+            </button>
+          </div>
+        </div>
+      </section>
+      <br />
+                  <a
+                    href="#theme"
+                    className="theme-btn"
+                    onClick={() => {
+                      // Implement your theme toggle logic here
+                      // Example: toggleTheme();
+                    }}
+                  >
                     <i className="fa-solid fa-moon"></i>
                   </a>
 
@@ -426,17 +430,17 @@ function App() {
                   </a>
 
                   <a href="#top" className="back-top-btn" aria-label="back to top" data-back-top-btn>
-                    <ion-icon name="caret-up-sharp" aria-hidden="true"></ion-icon>
+                    {/* <ion-icon name="caret-up-sharp" aria-hidden="true"></ion-icon> */}
                   </a>
                   <script src="./src/app.js"></script>
-                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+                  {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
                   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
                   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-                  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script> */}
 
-                </body>
+                </body >
 
 
 
@@ -444,4 +448,4 @@ function App() {
               )
 }
 
-              export default App
+export default App
